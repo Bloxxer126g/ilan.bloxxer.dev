2182d880202e55deca9466bee1b7ce2a94cce879
Bloxxer126g <tristan.raneroyay@proton.me>
2026-05-30 14:09:47 +0100

    Add base

--- global/redirect.js
+++
@@ -1,4 +0,0 @@
-let RedirectUrlParameters = new URLSearchParams(location.search)
-if (RedirectUrlParameters.get("accepted") !== "true") {
-    location.href = `https://ilan.bloxxer.dev/popup/?redirect=${location.href}`
-}
